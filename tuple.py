76eece97091a6aff8bb9f18270148a69f23396a2
hiteshkc <coolhkc@gmail.com>
2018-10-20 18:19:46 -0500

    memory and tuple

---
+++ tuple.py
@@ -0,0 +1,9 @@
+__author__ = 'hkc'
+
+class tuple(object):
+    def __init(self, action, obs, reward, info, done):
+        self.action = action
+        self.obs = obs
+        self.reward = reward
+        self.info = info
+        self.done = done
